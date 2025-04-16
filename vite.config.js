3df45d84870999f1cd05c8867d1a69cb936aef25
Yko-koshka <legezaanna7@gmail.com>
2025-04-16 07:47:47 +0200

    change the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production';
-
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-    base: isProd ? '/my-portfolio/' : '/',
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
 });
